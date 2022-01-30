041167eb5d2f0789cadc8555414bd233a3d990cb
Mariana Rocha <marianacrocha@ua.pt>
2022-01-30 01:04:56 +0000

    feito TUDO

--- src/Sobre.js
+++ src/Sobre.js
@@ -4,8 +4,10 @@ function Sobre() {
   return (
     <div className="App">
 
-<h1>SOBRE</h1>
-        <p>Esta página contém informação relativa ao nosso site</p>
+<h1 className="m-3">SOBRE</h1>
+        <p >Este site foi desenvolvido por dois alunos da Universidade de Aveiro, no âmbito da Unidade Curricular Laboratório Multimédia 5.</p>
+        <p >O site visa ser uma ferramenta útil para ver a metereologia de todas as cidades de Portugal de forma rápida e acessível.</p>
+        <img className="imgsobre mb-5" src="imagens/logo_ua.png"/>
     </div>
   );
 }
